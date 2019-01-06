ddd5e8df97a7c0ee3216ae2f5b803d4b53d2b87e
MichalWr88 <maleszewski.geo@gmail.com>
2019-01-06 13:06:25 +0100

    remove middelwear fn

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ app.use((req, res, next) => {
 	next();
 });
 //middelwear
-app.use((req, res, next) => {
-	res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+// 	res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
